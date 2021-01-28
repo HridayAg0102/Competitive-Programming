af3da482636a1c5c6458b2cfc354552e4f628128
hyturing <hyturing@gmail.com>
2021-01-29 01:06:00 +0530

    added comment

--- Codeforces/Contests/#697(Div-3)/b.cpp
+++ Codeforces/Contests/#697(Div-3)/b.cpp
@@ -3,6 +3,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Suppose a number n is given.
+// n = x.2020 + y.2021
+// n = (x+y).2020 + y
+/* Now y is kind of remainder when n is divided by 2020 y has to less than n//2020 because
+   then only it can be mixture of multiples of 2020 and 2021 */
+
 int main()
 {
 	long long int t;
